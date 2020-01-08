6deb80fc66f28b7bfbfcf2e5b842edad754d9b51
ehergert <59633201+ehergert@users.noreply.github.com>
2020-01-07 20:20:41 -0800

    Add files via upload

--- blockchain_API.py
+++ blockchain_API.py
@@ -8,3 +8,8 @@ VERSION=0.0
 import requests
 import datetime
 
+def GetBlock_Hash(block_hash):
+    url='http://blockchain.info/rawblock/{}?format=json'.format(block_hash)
+    with requests.Session() as s:
+        data=s.get(url).json()
+    return data

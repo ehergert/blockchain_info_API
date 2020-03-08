0f417b1640e7bbc4753bdeec1da454b1ad0c8879
ehergert <59633201+ehergert@users.noreply.github.com>
2020-03-08 14:56:57 -0700

    Update blockchain_API.py
    Update to add tx API, standardize function names

--- blockchain_API.py
+++ blockchain_API.py
@@ -1,15 +1,25 @@
 #
 # Blockchain.info API Library
 #
-#
+# Uses Blockchain.info's API to pull bitcoin blockchain data
+# Note: Blockchain.info does not currently support bc1 addresses
 
 VERSION=0.0
 
 import requests
 import datetime
 
-def GetBlock_Hash(block_hash):
+def Get_Block_By_Hash(block_hash):
     url='http://blockchain.info/rawblock/{}?format=json'.format(block_hash)
     with requests.Session() as s:
         data=s.get(url).json()
     return data
+
+def Get_Transaction_By_Hash(tx_hash)"
+    url='http://blockchain.info/rawtx/{}?format=json'.format(block_hash)
+    with requests.Session() as s:
+        data=s.get(url).json()
+    return data
+
+
+

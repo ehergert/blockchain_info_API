38524d9635f1d7f4d1e171985f1a0a822729425b
ehergert <59633201+ehergert@users.noreply.github.com>
2020-05-18 09:41:48 -0700

    Added function and fixed error

--- blockchain_API.py
+++ blockchain_API.py
@@ -15,8 +15,14 @@ def Get_Block_By_Hash(block_hash):
         data=s.get(url).json()
     return data
 
-def Get_Transaction_By_Hash(tx_hash)"
+def Get_Transaction_By_Hash(tx_hash):
     url='http://blockchain.info/rawtx/{}?format=json'.format(block_hash)
+    with requests.Session() as s:
+        data=s.get(url).json()
+    return data
+
+def Get_Single_Address_Info(address):
+    url='http://blockchain.info/rawaddr/{}?format=json'.format(address)
     with requests.Session() as s:
         data=s.get(url).json()
     return data
